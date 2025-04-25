86735c1a0326d92e2848f7564325a2bdd43ee8b5
EugenuSkok <eugenu0123456789@gmail.com>
2025-04-25 22:31:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+#1 Предсказание возраста по имени
+import requests 
+
+url = "https://api.agify.io/?name=michael"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+print(data)
+
+
+#2 Определение пола по имени
+import requests 
+
+url = "https://api.genderize.io/?name=luc"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+print(data)
+
+#3 Случайная фотография собаки
+import requests 
+
+url = "https://dog.ceo/api/breeds/image/random"
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+print(data)
 
